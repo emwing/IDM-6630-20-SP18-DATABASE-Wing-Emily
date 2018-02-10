3904f1a6c896120443144b680ac848e4401d42b0
emwing <emwing@emily-wing.com>
2018-02-09 23:36:49 -0500

    file and folder prep
    build docker-compose, Dockerfile, and Makefile according to demos; build
    index to bare minimum per demo

--- code/index.php
+++ code/index.php
@@ -1,5 +1,30 @@
 <?php
 
-echo "test";
+  $host = "localhost"; /* IP address (for MAMP, use "localhost") */
+  $db = "week03"; /* database name */
+  $un = "root"; /* username (for MAMP, use "root") */
+  $pw = "root"; /* password (for MAMP, use "root") */
 
- ?>
+  try {
+
+    $conn = new PDO("mysql:host=;dbname=$db", $un, $pw);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected Successfully";
+
+  } catch(PDOException $e) {
+
+    echo "Connection Failed: " . $e->getMessage();
+
+  }
+
+  $query = "SELECT * FROM /* week03 table name */";
+
+  echo "<ol>";
+
+  foreach ($conn->query($query) as $val) {
+    echo "<li>" . $val[/* 'column name' */] . "</li>";
+  }
+
+  echo "</ol>";
+
+?>
